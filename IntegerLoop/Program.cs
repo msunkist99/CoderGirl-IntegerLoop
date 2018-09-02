955bb6ab267f05a51516748d3f0a433fc28b71e5
Scott Kuhl <scott@kuhl.ws>
2018-09-02 01:30:26 -0500

    Add project files.

---
+++ IntegerLoop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntegerLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
