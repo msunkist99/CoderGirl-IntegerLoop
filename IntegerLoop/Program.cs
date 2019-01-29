19496be69604ab9b27466652cb92d28d13bab781
msunkist99 <msunkist99@gmail.com>
2019-01-28 18:22:17 -0600

    2019/01/29 Initial changs

--- IntegerLoop/Program.cs
+++ IntegerLoop/Program.cs
@@ -7,7 +7,13 @@ namespace IntegerLoop
         public static void Main()
         {
             // TODO: Print even integers from 0 to 50.
-
+            for (int i = 0; i < 51; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
             Console.ReadLine();
         }
     }
